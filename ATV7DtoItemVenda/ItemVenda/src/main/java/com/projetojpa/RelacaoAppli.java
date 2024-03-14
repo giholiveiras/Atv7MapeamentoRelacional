adceaef12f7122f5270691b6ad542bd915d69f58
giholiveiras <132359809+giholiveiras@users.noreply.github.com>
2024-03-14 15:28:32 -0300

    Add files via upload

---
+++ ATV7DtoItemVenda/ItemVenda/src/main/java/com/projetojpa/RelacaoAppli.java
@@ -0,0 +1,14 @@
+package com.projetojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelacaoAppli {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(RelacaoAppli.class, args);
+	}
+
+}
